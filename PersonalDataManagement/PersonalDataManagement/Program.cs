5192b26c566e752ebcf4a33000a48774a0682868
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-10 09:51:10 +0530

    [Prasanna] Add: Strcuture Folder Created

---
+++ PersonalDataManagement/PersonalDataManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalDataManagement
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Personal Data Management");
+        }
+    }
+}
